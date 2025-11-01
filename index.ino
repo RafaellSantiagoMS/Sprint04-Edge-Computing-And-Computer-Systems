2ed8563d4506a7efc4c39e0d5e978af3381c982c
Rafaell <rafaellsantiagoms@gmail.com>
2025-11-01 16:03:44 -0300

    FEAT: Adicionadas variáveis físicas e filtros para calibração

--- index.ino
+++ index.ino
@@ -22,3 +22,12 @@ const int LED = 2; // LED onboard
 WiFiClient espClient;
 PubSubClient MQTT(espClient);
 MPU6050 mpu;
+
+// =================== VARIÁVEIS FÍSICAS ===================
+float vel = 0.0;
+float dist = 0.0;
+unsigned long lastTime = 0;
+
+// Filtro e calibração
+float accX_f = 0, accY_f = 0, accZ_f = 0;
+float offsetX = 0, offsetY = 0, offsetZ = 0;
